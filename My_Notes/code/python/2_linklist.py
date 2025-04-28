46d704fc8f9e2938432a5c7b0dba605aa09baa24
clarkbeta <clarkinfbeta2022@163.com>
2025-04-28 22:29:39 +0800

    update files

--- My_Notes/code/python/2_linklist.py
+++ My_Notes/code/python/2_linklist.py
@@ -8,13 +8,13 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
-    def find_by_value(self, value):
+    def find_by_value(self, value: int):
         p = self.head
         while p and p.data != value:
             p = p.next
         return p
 
-    def find_by_index(self, index):
+    def find_by_index(self, index: int):
         p = self.head
         position = 0
         while p and position != index:
@@ -22,7 +22,11 @@ class LinkedList:
             position += 1
         return p
 
-    def insert_to_head(self, value):
-        node = Node(value)
-        node.next = self.head
-        self.head = node
+    def insert_before(self, index: int, node: Node, value: int):
+        if self.find_by_index(index) == self.head:
+            new_node = Node(value)
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            new_node = Node(value)
+            new_node.next = node.next
