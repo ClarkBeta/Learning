858816220d9a4b73845aa13dffab19fded3fdcf7
minos <clarkinfbeta2022@163.com>
2025-05-02 22:22:06 +0800

    update files

--- My_Notes/code/python/2_linklist.py
+++ My_Notes/code/python/2_linklist.py
@@ -22,11 +22,75 @@ class LinkedList:
             position += 1
         return p
 
-    def insert_before(self, index: int, node: Node, value: int):
-        if self.find_by_index(index) == self.head:
-            new_node = Node(value)
+    def insert_before(self, index: int, value: int):
+        new_node = Node(value)
+        if index == 0:
             new_node.next = self.head
             self.head = new_node
         else:
-            new_node = Node(value)
-            new_node.next = node.next
+            current = self.head
+            position = 0
+            while current and position < index - 1:
+                current = current.next
+                position += 1
+            if not current:
+                raise IndexError("Index out of range")
+            new_node.next = current.next
+            current.next = new_node
+
+    def delete_by_index(self, index: int):
+        if not self.head:
+            raise IndexError("Index out of range (empty list)")
+        if index == 0:
+            self.head = self.head.next
+        else:
+            current = self.head
+            position = 0
+            while current and position < index - 1:
+                current = current.next
+                position += 1
+            if not current or not current.next:
+                raise IndexError("Index out of range")
+            current.next = current.next.next
+
+    def delete_by_value(self, value: int):
+        if not self.head or not value:
+            return
+        if self.head.data == value:
+            self.head = self.head.next
+            return
+        pre = self.head
+        node = self.head.next
+        not_found = False
+        while node.data != value:
+            if node.next:
+                not_found = True
+                break
+            else:
+                pre = node
+                node = node.next
+        if not not_found:
+            pre.next = node.next
+
+    def print_all(self):
+        if not self.head:
+            print("Empty list")
+            return
+        current = self.head
+        print(f"{current.data}", end="")
+        current = current.next
+        while current:
+            print(f"->{current.data}", end="")
+            current = current.next
+        print()
+
+
+if __name__ == '__main__':
+    llist = LinkedList()
+    llist.insert_before(0, 1)  # 链表: 1
+    llist.insert_before(0, 2)  # 链表: 2->1
+    llist.insert_before(1, 3)  # 链表: 2->3->1
+    llist.insert_before(3, 4)  # 链表: 2->3->1->4
+    llist.delete_by_index(0)  # 删除 2，链表变为 3->1->4
+    llist.delete_by_value(3)
+    llist.print_all()  # 输出: 3->1->4
